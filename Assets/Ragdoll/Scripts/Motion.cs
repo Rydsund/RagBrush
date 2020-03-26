bf8a7104ccfda3673e258178cb9387691694c252
johanmelkersson <melkersson.j@gmail.com>
2020-03-26 09:33:37 +0100

    Initial commit

---
+++ Assets/Ragdoll/Scripts/Motion.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+	private HingeJoint Hj;
+	public Transform legAnimation;
+	public bool Mirror;
+    void Start()
+    {
+		Hj = GetComponent<HingeJoint>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		if (legAnimation != null)
+		{
+			JointSpring js = Hj.spring;
+			js.targetPosition = legAnimation.localEulerAngles.x;
+			if (js.targetPosition > 180)
+			{
+				js.targetPosition = js.targetPosition - 360;
+			}
+			js.targetPosition = Mathf.Clamp(js.targetPosition, Hj.limits.min + 5, Hj.limits.max - 5);
+			if (Mirror)
+			{
+				js.targetPosition = js.targetPosition *= -1;
+			}
+			Hj.spring = js;
+		}
+    }
+}
