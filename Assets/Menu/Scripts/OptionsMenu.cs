1db2a8a6446ff210d16d1749ec4da61191311ad9
johanmelkersson <melkersson.j@gmail.com>
2020-04-18 03:26:16 +0200

    test

---
+++ Assets/Menu/Scripts/OptionsMenu.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    public AudioMixer audioMixer;
+
+    public void SetVolume(float volume)
+    {
+        audioMixer.SetFloat("Volume", volume);
+        Debug.Log(volume);
+    }
+
+    public void SetQuality(int qualityIndex)
+    {
+        QualitySettings.SetQualityLevel(qualityIndex);
+    }
+}
