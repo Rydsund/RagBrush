ddcebef56ebcb4237ad3c998394a25e1e3f78a71
johanmelkersson <melkersson.j@gmail.com>
2020-04-23 01:45:35 +0200

    PausMenu

---
+++ Assets/Menu/Scripts/PausMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PausMenu : MonoBehaviour
+{
+
+    public static bool gamePaused = false;
+
+    public GameObject pausMenuUI;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (gamePaused)
+            {
+                Resume();
+            }
+            else
+            {
+                Paus();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        pausMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+        gamePaused = false;
+    }
+
+    void Paus()
+    {
+        pausMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        Cursor.visible = true;
+        Cursor.lockState = CursorLockMode.None;
+        gamePaused = true;
+    }
+
+
+    public void ExitToMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Quit");
+    }
+}
