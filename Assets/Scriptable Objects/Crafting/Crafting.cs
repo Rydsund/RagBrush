cd4954cf2f4197782a6df450b625455bdcd23279
Rydsund <62593116+Rydsund@users.noreply.github.com>
2020-04-22 11:10:32 +0200

    Saving first draft to Crafting

--- Assets/Scriptable Objects/Crafting/Crafting.cs
+++ Assets/Scriptable Objects/Crafting/Crafting.cs
@@ -1,24 +1,57 @@
-﻿using System.Collections;
+﻿using JetBrains.Annotations;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.IO;
+using System.Linq;
 using UnityEngine;
 
 public class Crafting : MonoBehaviour
 {
-    
+    public Recipe[] craftingRecipies;
+
     void Start() // För att läsa in recept.
     {
-        
+        LoadRecipies();
     }
 
 
     public void Craft(InventorySlot slot1, InventorySlot slot2) // För crafting.. duh.
     {
+        List<InventorySlot> craftingSlots = new List<InventorySlot>();
+        craftingSlots.Add(slot1);
+        craftingSlots.Add(slot2);
+
+        //for (int i = 0; i < 1; i++)
+        //{
+        //    if (craftingSlots[i] == )
+        //    {
+
+        //    }
+        //}
+
+        // Ett första utkast av craftinglogiken.
+        if (craftingRecipies[0].inputObj1 == craftingSlots[0].item.objectPrefab
+            && craftingRecipies[0].inputObj2 == craftingSlots[1].item.objectPrefab
+            || craftingRecipies[0].inputObj1 == craftingSlots[1].item.objectPrefab
+            && craftingRecipies[0].inputObj2 == craftingSlots[0].item.objectPrefab)
+        {
+            Instantiate(craftingRecipies[0].outputObj, this.transform);
+        }
+
         /*
          * loopar igenom player inventory
          * kollar om contents är lika med input objekt för recept
-         * om ja spotta ut item.
-                    
+         * om ja spotta ut item. 
          */
+                    
+         
+    }
+
+    private void LoadRecipies()
+    {
+        craftingRecipies = Resources.LoadAll<Recipe>("");
+        Debug.Log(craftingRecipies.Length);
+        Debug.Log(craftingRecipies[0].name);
     }
 }
