d997985babe07d944f4c0dd879574318b76042ab
Rydsund <62593116+Rydsund@users.noreply.github.com>
2020-05-13 11:10:22 +0200

    Saving changes

---
+++ Assets/Prefabs/ItemPrefabs/DestroyedItems/DestroyedObjectTimer.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyedObjectTimer : MonoBehaviour
+{
+    // Viktor.
+    /// <summary>
+    /// Class is a simple timer for destructible objects
+    /// </summary>
+
+    float objectLifeTime;
+    Random random;
+    void Start()
+    {
+        objectLifeTime = Random.Range(15, 30);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        objectLifeTime -= Time.deltaTime;
+        if (objectLifeTime <= 0)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
