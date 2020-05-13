8c885c430e2293a7e6a521a6c4123a59280153dd
johanmelkersson <melkersson.j@gmail.com>
2020-05-13 02:49:20 +0200

    lastfix

--- Assets/Scripts/Sun.cs
+++ Assets/Scripts/Sun.cs
@@ -2,15 +2,38 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// 
+/// /Johan
+/// </summary>
 public class Sun : MonoBehaviour
 {
+    Light sunShine;
 
     [SerializeField]
-    private float daytime = 1;
+    private float dayTime = 1;
+
+    private void Start()
+    {
+        sunShine = GetComponent<Light>();
+    }
+
 
     void Update()
     {
-        transform.RotateAround(Vector3.zero, Vector3.right, daytime * Time.deltaTime);
+        transform.RotateAround(Vector3.zero, Vector3.right, dayTime * Time.deltaTime);
         transform.LookAt(Vector3.zero);
+
+        if (transform.position.y > -150 && sunShine.intensity < 1f)
+        {
+            sunShine.intensity += 0.1f;
+        }
+        else if (transform.position.y < -150 && sunShine.intensity > 0.1f)
+        {
+            sunShine.intensity -= 0.1f;
+        }
     }
+    
+    
 }
